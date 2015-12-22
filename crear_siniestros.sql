f33b8b538bcc82bd5e8f5fa712a37e3e0f55170b
ivangbodnar <ivangbodnar@gmail.com>
2015-12-22 20:49:35 -0300

    segundo

---
+++ crear_siniestros.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE TABLE siniestros (
+
+	id BIGSERIAL PRIMARY KEY,
+	inspectores varchar(100),
+	fecha date,
+	hora_llegada time,
+	calle_1 varchar(150),
+	calle_2 varchar(150),
+	altura integer
+
+);
+
+SELECT * FROM siniestros;
+
+DROP TABLE siniestros;
